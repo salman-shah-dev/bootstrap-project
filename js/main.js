1c90b782d69d4c40327922eaf3c6ad511ac1e91d
salman-shah-dev <149875739+salman-shah-dev@users.noreply.github.com>
2023-11-05 19:17:09 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.main-slider').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows:true,
+    prevArrow : '<i class="fa-solid fa-angle-left prev"></i>',
+    nextArrow : '<i class="fa-solid fa-angle-right next"></i>'
+});
